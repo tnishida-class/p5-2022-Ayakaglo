5fbb300a38adee00ea0f1e27f27996fc9b6ac0e5
Ayakaglo <2050511h@stu.kobe-u.ac.jp>
2022-10-18 12:40:40 +0900

    changes section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,16 +1,33 @@
-// 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
-  cycle = 100;
+  cycle = 150;
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
+ if(keyIsDown(" ".charCodeAt(0))){{if(count>80) count = (count -20) % cycle;
+  
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+ }
+  if(count>30){count = (count + 3) % cycle;}
+else{count = (count + 1) % cycle;}
+  fill(255,0,0);
+  ellipse(width / 2, height / 2, count);
+      }
+
+  else{
+    if(count>80) {count = (count -50) % cycle;
+  
+  // BLANK[1]
+ }
+  if(count>30){count = (count + 3) % cycle;}
+else{count = (count + 1) % cycle;}
+  fill(255,0,0);
+  ellipse(width / 2, height / 2, count);
+      
+  }
 }
