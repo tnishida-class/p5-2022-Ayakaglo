2539037411a297d8fdfdff46304d04e8cc187d78
Ayakaglo <2050511h@stu.kobe-u.ac.jp>
2022-10-25 12:03:58 +0900

    changes section4-1

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -1,35 +1,39 @@
-// テキスト「配列」～「配列を使った描画」までを収録
 function setup(){
   createCanvas(400, 400);
   background(240);
 
-  // 配列をランダムに初期化する
+  
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(20, 100); // 20以上100未満のランダムな数を代入
   }
   console.log(scores);
 
-  // 合計を計算する
+ 
   let sum = 0;
   for(let i = 0; i < scores.length; i++){
     sum += scores[i];
   }
   console.log(sum);
 
-  // ここから平均・最大・最小を求めます
-  let average, largest, smallest;
-  // BLANK[1]　平均値（ヒント average = 合計 / 配列の長さ）
+    let largest, smallest;
+  let average = sum/scores.length
+  console.log(average);
+
 
   largest = 0;
   for(let i = 0; i < scores.length; i++){
-    // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
+    if(largest<=scores[i]){largest = scores[i] // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
   }
+  else{largest == largest}}
+  console.log(largest);
 
   smallest = 100;
   for(let i = 0; i < scores.length; i++){
-    // BLANK[3]　ヒント：最小値とだいたい同じ
+    if(smallest>=scores[i]){smallest = scores[i] // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
   }
+  else{smallest == smallest}}
+  console.log(smallest);
 
   // ここから棒グラフを描いていきます。まずは背景に横線をn本引く
   const n = 10;
@@ -40,11 +44,20 @@ function setup(){
   for(let i = 0; i < scores.length; i++){
     const dx = width / scores.length;
     const h = height * scores[i] / 100;
+   
+    if(scores[i] == largest){fill(255,0,0);
+      rect(i * dx + 2, height - h, dx - 4, h);  }
+    else if (scores[i] == smallest){fill(0,0,255);
+      rect(i * dx + 2, height - h, dx - 4, h);  }
+    else{fill(0);
+      rect(i * dx + 2, height - h, dx - 4, h);  }
     // BLANK[4] ヒント: 条件分岐を使って色を変更します
-    rect(i * dx + 2, height - h, dx - 4, h);
-    fill(0);
+         
     text(scores[i].toPrecision(3), i * dx, height - h);
   }
+  //平均値入れる
+  
+  stroke(0,255,0);
+  line(0, height-height*average/100,width, height-height*average/100);
 
-  // BLANK[5] 平均点の線を引きます
 }
