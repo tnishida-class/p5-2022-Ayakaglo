b6d251e8fb1e75c887bd76066ad8cbbc51519e30
Ayakaglo <2050511h@stu.kobe-u.ac.jp>
2022-11-14 18:23:16 +0900

    Changes section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,17 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46",50,40,255,0,0);
 }
 
-function balloon(t){
+function balloon(t,x,y,cr,cg,cb){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+  fill(cr,cg,cb);
+  rect(x, y, w + p * 2, h + p * 2);
+  fill(cr,cg+255,cb);
+  text(t, p+x, h + p+y);
+  fill(cr-255,cg,cb+255)
+  triangle(x+w + p * 2-30, y + h + p * 2, x+w + p * 2,y+h+p * 2,x+w + p * 2-20,y + h + p * 2+20);
 }
